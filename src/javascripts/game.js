87aa9975c195408a92cc31af32fbb61104ca9474
Eduardo Rodrigues Oliveira <159492869+eduardohro@users.noreply.github.com>
2025-03-31 19:30:17 -0300

    update map

--- src/javascripts/game.js
+++ src/javascripts/game.js
@@ -11,6 +11,9 @@ let createRect = (x, y, width, height, color) => {
 let fps = 30;
 let oneBlockSize = 20;
 let wallColor = "#342DCA";
+let wallSpaceWidth = oneBlockSize / 1.5;
+let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
+let wallInnerColor = "black";
 
 let map = [
     [1,1,1,1,1 ,1,1,1,1,1 , 1,1,1,1,1, 1,1,1,1,1, 1],
@@ -48,6 +51,7 @@ let update = () => {
 };
 
 let draw = () => {
+    createRect(0,0, canvas.width, canvas.height, "black")
     //todo
     drawWalls()
 };
@@ -58,7 +62,23 @@ let drawWalls = () => {
     for(let i = 0; i < map.length; i++) {
         for(let j = 0; j < map[0].length; j++) {
             if (map[i][j] == 1) { // then it is a wall
-                createRect(j * oneBlockSize, i * oneBlockSize, oneBlockSize, oneBlockSize, wallColor)
+                createRect(j * oneBlockSize, i * oneBlockSize, oneBlockSize, oneBlockSize, wallColor);
+
+                if (j > 0 && map[i][j - 1] == 1) {
+                    createRect(j * oneBlockSize, i * oneBlockSize + wallOffset, wallSpaceWidth + wallOffset, wallSpaceWidth, wallInnerColor);
+                }
+
+                if (j < map[0].length - 1 && map[i][j + 1] == 1) {
+                    createRect(j * oneBlockSize + wallOffset, i * oneBlockSize + wallOffset, wallSpaceWidth + wallOffset, wallSpaceWidth, wallInnerColor);
+                }
+
+                if (i > 0 && map[i - 1][j] == 1) {
+                    createRect(j * oneBlockSize + wallOffset, i * oneBlockSize, wallSpaceWidth, wallSpaceWidth + wallOffset, wallInnerColor);
+                }
+
+                if (i < map.length - 1 && map[i + 1][j] == 1) {
+                    createRect(j * oneBlockSize + wallOffset, i * oneBlockSize + wallOffset, wallSpaceWidth, wallSpaceWidth + wallOffset, wallInnerColor);
+                }
             }
         }
     }
